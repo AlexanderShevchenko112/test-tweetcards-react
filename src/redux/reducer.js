bdd4a534c86556880b4e6c6c00682376c8d5ec53
AlexanderShevchenko112 <aleks.shevchenko.2013@gmail.com>
2023-05-19 21:35:51 +0300

    removed redux

--- src/redux/reducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import { usersReducer } from "./usersSlice";
-import { followerReducer } from "./followerSlice";
-
-export const reducer = combineReducers({
-  users: usersReducer,
-  followerOf: followerReducer,
-});
